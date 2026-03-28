e611d3b0424705d9a1fedc68e6e2716873b92721
H1101241014-Naomy <h1101241014@student.untan.ac.id>
2026-03-28 18:54:59 +0700

    fix oauth debug ver 2

--- apps/backend/src/auth.ts
+++ apps/backend/src/auth.ts
@@ -1,10 +1,13 @@
 import { google } from "googleapis";
 
 export function createOAuthClient() {
+  console.log("CLIENT ID:", process.env.GOOGLE_CLIENT_ID),
+    console.log("REDIRECT:", process.env.GOOGLE_REDIRECT_URI);
+    
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI
+    process.env.GOOGLE_REDIRECT_URI,
   );
 }
 
